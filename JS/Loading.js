50f388937c69d4b11e465a0fdf1f109729a60fd7
NguyenLannnFE <nguyenhoanglan0311@gmail.com>
2025-06-04 21:53:19 +0700

    1

--- JS/Loading.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("load", () =>{
-    const preloader = document.getElementById("preloader")
-    const content = document.getElementById("max-content")
-
-    setTimeout(() =>{
-        preloader.style.display = "none"
-        content.style.display = "block"
-    },1000)
-})
